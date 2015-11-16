2b9df882f4616eeeca78cf93d5e1d044675a9670
stefanie <srfritz@uvic.ca>
2015-11-16 14:37:13 -0800

    Simple View for Assignment
    view for assignments,
    added a due date for the assignment

--- assignments/models.py
+++ assignments/models.py
@@ -4,6 +4,7 @@ class Assignment(models.Model):
     name = models.CharField(max_length=255)
     section = models.ForeignKey('courses.Section')
     description = models.TextField()
+    due_date = models.DateTimeField()
     
     def __str__(self):
         return str(self.section) + " - " + self.name
