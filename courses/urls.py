6ef57f8f734c9802e13ca2dd7f3ff191f831fa24
Jimmy Pardey <jpardey@gmail.com>
2015-11-25 13:16:34 -0800

    Admin users can now add courses

--- courses/urls.py
+++ courses/urls.py
@@ -8,4 +8,5 @@ urlpatterns = [
     url(r'^section/(?P<id>[0-9]+)/$', views.section_detail, name='section_detail'),
     url(r'^section/(?P<section_id>[0-9]+)/assignments/$', views.assignment, name='assignment_index'),
     url(r'^section/assignments/(?P<assignment_id>[0-9]+)$', views.assignment_detail, name='assignment_detail'),
+    url(r'^add_course/$', views.add_course, name="add_course")
 ]
