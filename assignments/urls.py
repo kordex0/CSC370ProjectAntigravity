1629565df6d6097bbbe2717f283f1fb83c9e99bc
stefanie <srfritz@uvic.ca>
2015-11-25 16:52:53 -0800

    save state

--- assignments/urls.py
+++ assignments/urls.py
@@ -6,7 +6,7 @@ urlpatterns = [
     # first: regex
     # second: calls specified view function with an HttpRequest object as the first argument and any captured values from the regular expresion: ?P<...>
     # third: name the url for better use in templates
-    url(r'^$', views.index, name='index'),
+    url(r'^/$', views.index, name='index'),
     # ?P<id> pass id as keyword argument to function views.course
     url(r'^(?P<id>[0-9]+)/$', views.assignment, name='detail'),
 ]
