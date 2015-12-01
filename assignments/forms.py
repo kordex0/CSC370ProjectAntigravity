6f769a91ab2c87b696c66edf24dfb1acd2d6322c
Jimmy Pardey <jpardey@gmail.com>
2015-11-30 22:00:59 -0800

    Testing datetime widgets

--- assignments/forms.py
+++ assignments/forms.py
@@ -5,10 +5,13 @@ from .models import Assignment, Submission
 from users.models import User
 from courses.models import Course, Section
 
+import datetime
+
 class NewAssignmentForm(forms.Form):
     name = forms.CharField(max_length=255)
     description = forms.CharField(widget=forms.Textarea)
-    due_date = forms.DateTimeField(widget=SelectDateWidget)
+    due_date = forms.DateField(widget=SelectDateWidget)
+    due_time = forms.TimeField(default=datetime.time(17))
 
 class AssignmentSubmissionForm(forms.Form):
     submission = forms.CharField(widget=forms.Textarea)
